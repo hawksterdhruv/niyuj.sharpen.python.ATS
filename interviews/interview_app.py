0f6f3a3802879d15a439c16717f614ce07452483
anuragpathak2608 <anuragpathak2608@gmail.com>
2020-02-20 12:31:31 +0530

    Added API to fetch interview based on days(find interviews scheduled in next 7 days)

--- interviews/interview_app.py
+++ interviews/interview_app.py
@@ -2,6 +2,7 @@ from SetupDb import db_session
 import SetupDb
 from Model import Interview
 import datetime
+from datetime import datetime, timedelta
 
 from flask import Flask
 from flask import request
@@ -56,6 +57,17 @@ def update_interview(id):
    interviewObj = interviewUpdatedObj
    db_session.commit()
    return {"id": interviewObj.id,"status": "OK"}
+
+
+@app.route('/interviews/days/<days>')
+def get_interview_by_date(days):
+    current_date = datetime.now()
+    search_date = datetime.now() + timedelta(days=int(days))
+    results = db_session.query(Interview).filter(Interview.schedule_time.between(current_date, search_date))
+    date_list = []
+    for value in results:
+        date_list.append(value.serialize())
+    return jsonify(date_list)
 
 
 if __name__ == '__main__':
