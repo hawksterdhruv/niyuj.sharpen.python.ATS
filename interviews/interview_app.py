3516894e1d08914f54d6b4357feff55005dd3f7f
Umang Agrawal <umangagrawal1712@gmail.com>
2020-02-28 11:59:22 +0530

    Use port 4000 for inerview app, Use CORS.

--- interviews/interview_app.py
+++ interviews/interview_app.py
@@ -9,11 +9,11 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 import json
-#from flask_cors import CORS
+from flask_cors import CORS
 app = Flask(__name__)
-#CORS(app)
+CORS(app)
 
-SetupDb.init_db()
+#SetupDb.init_db()
 # dummyInterviewdata = Interview(2,2,"F2F", "Niyuj HQ", "NA", "NA", datetime.datetime.now())
 #
 # db_session.add(dummyInterviewdata)
@@ -89,4 +89,4 @@ def patch_interview(id):
 
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(port=4000)
